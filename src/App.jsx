496cbc878f6f7f4240005effc74329a7eb295681
poorvith519 <poorvith519@gmail.com>
2026-03-19 07:06:16 +0530

    Create src/App.jsx with the CypherCap component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CypherCap = () => {
+    return (
+        <div>
+            <h1>CypherCap Component</h1>
+            <p>This is the CypherCap component.</p>
+        </div>
+    );
+};
+
+export default CypherCap;
